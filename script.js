6618e91de4cf17792787e78dc32f3c4c78837108
Cadence K <cadencehiggins@gmail.com>
2024-04-23 18:32:23 -0500

    filled HTML, started with javaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+
+const choices = ['rock', 'paper', 'scissors']
+const playerChoice = document.getElementById('player-choice')
+const opponentChoice = document.getElementById('opponent-choice')
+const result = document.getElementById('result')
